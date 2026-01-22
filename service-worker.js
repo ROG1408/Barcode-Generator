dedc1b3489595d0f55bebd779d67b8d67f7ec90e
ROG1408 <rog.sj408@gmail.com>
2026-01-21 19:23:47 -0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("barcodegen-v1").then(cache => {
+    caches.open("barcodegen-v2").then(cache => {
       return cache.addAll([
         "./",
         "./index.html",
@@ -19,3 +19,4 @@ self.addEventListener("fetch", event => {
     })
   );
 });
+
